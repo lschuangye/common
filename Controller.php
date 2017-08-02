e5b25bd1d2eaf1f3cce3aa4131bedc2a1f9c47fa
yangzongcheng <1432704393@qq.com>
2017-08-02 19:46:51 +0800

    2017年8月2日19:46:48

--- Controller.php
+++ Controller.php
@@ -5,6 +5,30 @@
  class Controller{
 
 
+
+
+     /**
+      * @param $multi_array
+      * @param $sort_key
+      * @param int $sort
+      * @return bool
+      * 二维数组排序
+      */
+     protected function multi_array_sort($multi_array,$sort_key,$sort=SORT_ASC){
+         if(is_array($multi_array)){
+             foreach ($multi_array as $row_array){
+                 if(is_array($row_array)){
+                     $key_array[] = $row_array[$sort_key];
+                 }else{
+                     return false;
+                 }
+             }
+         }else{
+             return false;
+         }
+         array_multisort($key_array,$sort,$multi_array);
+         return $multi_array;
+     }
      /**
       * @param $page
       * @param $limit
