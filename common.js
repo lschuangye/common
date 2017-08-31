6c5959b4575de90b4ade592ea5737bd5f67a3336
yzc <1432704383@qq.com>
2017-08-31 13:27:58 +0800

    2017年08月31日13:27:54

--- common.js
+++ common.js
@@ -2,6 +2,33 @@
  * Created by yangzc on 2017/7/28.
  * 公用js库
  */
-function ajax(){
+/**
+ * 验证ajax 返回参数
+ * @param $data
+ */
+function check_data($data){
+    if($data.errcode!=200){
+        return layer.msg($data.msg);
+    }else{
+        return $data.data;
+    }
+}
 
+/***
+ * jq ajaxpost
+ * @param url
+ * @param data
+ * @param call_function
+ */
+function ajax_post(url,data,call_function){
+    $.ajax({
+        url:url,
+        type:'post',
+        data:data,
+        dataType:'json',
+        success:call_function,
+        error:function(){
+            return layer.msg('请求失败');
+        }
+    });
 }
