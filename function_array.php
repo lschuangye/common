30c99e9b1b713bab072859c009afbf6338682326
yzc <1432704383@qq.com>
2017-12-28 09:10:43 +0800

    2017年12月28日09:10:39

--- function_array.php
+++ function_array.php
@@ -92,3 +92,14 @@ function setNullData($data){
 
     return $data;
 }
+
+/*
+ * XML文件转换为数组
+ */
+function xmlToArray($xml)
+{
+    //禁止引用外部xml实体
+    libxml_disable_entity_loader(true);
+    $values = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+    return $values;
+}
