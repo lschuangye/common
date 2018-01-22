ffa5551d5fbffbf706cc1b1a31aff556bfc84830
yzc <1432704383@qq.com>
2018-01-22 12:13:51 +0800

    文件下载

--- function_file.php
+++ function_file.php
@@ -4,16 +4,19 @@
  * User: yangzc
  * Date: 2018/1/19
  * Time: 18:14
- * 文件 操作函数 
+ * 文件 操作函数
  */
 
 
 /**
  * @param $filename 文件路径
  * @param $name 下载的文件名 包括后缀
+ * $type 为文件类型  如 pdf  doc 等
  */
-function download_file($filename,$name){
-//    header('Content-type: application/pdf');
+function download_file($filename,$name,$type=''){
+    if($type){
+        header('Content-type: application/'.$type);
+    }
 
     header('Content-Disposition: attachment; filename="'.$name.'"');
     readfile($filename);
